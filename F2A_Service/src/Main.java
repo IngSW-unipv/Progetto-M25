b6f937de02b9d9250487066f6d5a427a212fcb93
AndreGiova27 <andreagiovanni.padovano01@universitadipavia.it>
2024-12-13 16:29:20 +0100

    prova andregiova27

--- F2A_Service/src/Main.java
+++ F2A_Service/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("ssss");
 		System.out.println("ciaooooooooo");
+		System.out.println("come va??");
 		// TODO Auto-generated method stub
 
 	}
