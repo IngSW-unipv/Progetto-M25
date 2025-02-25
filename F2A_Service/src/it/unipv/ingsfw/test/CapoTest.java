8c5fc4affa46af17c391d259f9a21b16ef20a545
AndreGiova27 <andreagiovanni.padovano01@universitadipavia.it>
2025-02-25 16:16:12 +0100

    lavoro 25/02
    sistemazione errore

---
+++ F2A_Service/src/it/unipv/ingsfw/test/CapoTest.java
@@ -0,0 +1,18 @@
+package it.unipv.ingsfw.test;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CapoTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ F2A_Service/src/it/unipv/ingsfw/test/CorriereTest.java
@@ -0,0 +1,19 @@
+package it.unipv.ingsfw.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CorriereTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
